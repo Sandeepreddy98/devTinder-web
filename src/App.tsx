06010732b10000cefa0225d6fa9651742a897bef
Sandeep <g.sandeepreddy98@gmail.com>
2025-04-15 16:00:11 +0530

    [NEW] Routes Declaration Description: 1)Installed react-router using npm. 2)Created BrowserRouter,Routes,Route & child Route for all the component. 3)Created Outlet in body b/w navbar,footer. 4) Created footer component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import NavBar from "./components/Navbar"
+import { BrowserRouter, Route, Routes } from "react-router";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Profile from "./components/Profile";
 
 function App() {
-
   return (
-    <NavBar/>
-  )
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
