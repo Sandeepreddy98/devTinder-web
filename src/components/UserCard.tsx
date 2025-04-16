232ea1ae9d7f5bbd7be5e1555c0a69734a3822bc
Sandeep <g.sandeepreddy98@gmail.com>
2025-04-16 23:47:45 +0530

    [FIX] Minor Fixed. Description: Min & max width for image  and abourt section in UserCard.

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,16 +1,17 @@
 function UserCard({user} : any){
-    const {age,firstName,lastName,gender,photos,skills} = user
+    const {age,firstName,lastName,gender,photos,skills,about} = user
     
     return <div className="card bg-base-300 w-96 shadow-sm">
     <figure className="px-10 pt-10">
       <img
         src={photos[0]}
         alt={firstName + " "+ lastName + " photo"}
-        className="rounded-xl" />
+        className="rounded-xl max-h-52 max-w-52" />
     </figure>
     <div className="card-body items-center text-center">
       <h2 className="card-title">{firstName + " " + lastName}</h2>
       <p>{age && gender && age +"," + gender}</p>
+      <p>{about}</p>
       <div className="card-actions flex justify-between gap-10 mt-5">
         <button className="btn btn-primary w-25">Ignore</button>
         <button className="btn btn-secondary w-25">Interested</button>
